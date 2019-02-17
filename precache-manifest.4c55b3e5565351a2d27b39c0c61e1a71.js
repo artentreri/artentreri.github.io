1ba2df83a555803f232234847ceef547bddb27c8
Artemis Entreri <deusmortale@gmail.com>
2019-02-17 05:50:27 +0300

    Updates

---
+++ precache-manifest.4c55b3e5565351a2d27b39c0c61e1a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "46c60788d5c090547e7b",
+    "url": "/static/js/main.46c60788.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "46c60788d5c090547e7b",
+    "url": "/static/css/main.956ebfdd.chunk.css"
+  },
+  {
+    "revision": "e661cbb413bd239d9d36ea0adf02b06e",
+    "url": "/index.html"
+  }
+];
